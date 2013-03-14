4ee62ba50dc7c8f419afd7cd26f940a330692bb5
spadasin <samor@qatar.cmu.edu>
2013-03-14 20:35:41 +0800

    Update local.js

--- static/local.js
+++ static/local.js
@@ -1,26 +1,11 @@
 function TodoCtrl($scope) {
-  $scope.todos = [
-    {text:'<font color="red">This is some text!</font>', done:true},
-    {text:'build an angular app', done:false}];
+  $scope.todos = [];
  
   $scope.addTodo = function() {
-    $scope.todos.push({text:$scope.todoText, done:false});
+    $scope.todos.push({text:$scope.todoText});
     $scope.todoText = '';
   };
  
-  $scope.remaining = function() {
-    var count = 0;
-    angular.forEach($scope.todos, function(todo) {
-      count += todo.done ? 0 : 1;
-    });
-    return count;
-  };
  
-  $scope.archive = function() {
-    var oldTodos = $scope.todos;
-    $scope.todos = [];
-    angular.forEach(oldTodos, function(todo) {
-      if (!todo.done) $scope.todos.push(todo);
-    });
-  };
-}
+ 
+  }
